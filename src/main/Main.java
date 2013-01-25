7b4cb71a772b3e0e9869938c36bbfb3e744b909d
Arinto Murdopo <arinto@gmail.com>
2013-01-25 12:31:54 +0100

    Add a classifier and two data streams

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,19 @@
 package main;
 
-public class Main {
-    
-    public static void main(String[] args){
-        System.out.println("Main class!");
-    }	
+import moa.classifiers.Classifier;
+import moa.classifiers.trees.HoeffdingTree;
+import moa.streams.generators.RandomRBFGeneratorDrift;
+import moa.streams.generators.RandomTreeGenerator;
 
+public class Main {
+	public static void main(String[] args) {
+
+		Classifier learner = new HoeffdingTree();
+		RandomTreeGenerator rtStream = new RandomTreeGenerator();
+		rtStream.prepareForUse();
+
+		RandomRBFGeneratorDrift rbfDriftStream = new RandomRBFGeneratorDrift();
+		rbfDriftStream.speedChangeOption.setValue(0.001);
+		rbfDriftStream.prepareForUse();
+	}
 }
